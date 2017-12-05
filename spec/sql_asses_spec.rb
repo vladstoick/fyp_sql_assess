114a4188df1b328d8a508dae494f8cc5a16a1c46
Vlad Stoica <vlad96stoica@gmail.com>
2017-12-05 15:01:05 +0000

    Remove initial spec that fails

--- spec/sql_asses_spec.rb
+++ spec/sql_asses_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe SqlAsses do
   it "has a version number" do
     expect(SqlAsses::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
