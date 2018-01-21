c0bd2ed6fb4fe1f9db615a31914afbe40f665ee1
Vlad Stoica <vlad96stoica@gmail.com>
2018-01-21 18:25:42 +0000

    Remove if branch that checks for database

--- lib/sql_assess/database_connection.rb
+++ lib/sql_assess/database_connection.rb
@@ -5,21 +5,15 @@ module SqlAssess
     def initialize(host: "127.0.0.1", port: "3306", username: "root", database: nil)
       if database.nil?
         database = "local_db"
-        @client = Mysql2::Client.new(
-          host: host,
-          port: port,
-          username: username,
-          init_command: "CREATE DATABASE IF NOT EXISTS #{database}"
-        )
-        @client.select_db(database)
-      else
-        @client = Mysql2::Client.new(
-          host: host,
-          port: port,
-          username: username,
-          database: database,
-        )
       end
+      @client = Mysql2::Client.new(
+        host: host,
+        port: port,
+        username: username,
+        init_command: "CREATE DATABASE IF NOT EXISTS #{database}"
+      )
+
+      @client.select_db(database)
     rescue Mysql2::Error => exception
       raise DatabaseConnectionError.new(exception.message)
     end
