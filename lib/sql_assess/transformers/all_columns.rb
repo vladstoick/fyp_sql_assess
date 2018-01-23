40fcb528b26648955319dda5c2634def44925f04
Vlad Stoica <vlad96stoica@gmail.com>
2018-01-23 15:24:03 +0000

    Transform * select list

---
+++ lib/sql_assess/transformers/all_columns.rb
@@ -0,0 +1,35 @@
+require 'sql-parser'
+
+module SqlAssess::Transformers
+  class AllColumns < Base
+    def transform(query)
+      @parsed_query = @parser.scan_str(query)
+
+      if @parsed_query.query_expression.list.is_a?(SQLParser::Statement::All)
+        transform_star_select
+        @parsed_query.to_sql
+      else
+        @parsed_query.to_sql
+      end
+    end
+
+    private
+
+    def transform_star_select
+      table_expression = @parsed_query.query_expression.table_expression.to_sql
+      columns_query = "SHOW columns #{table_expression}"
+      column_names = @connection.query(columns_query).map { |k| k["Field"] }
+
+      sql_columns = column_names.map do |column_name|
+        SQLParser::Statement::Column.new(column_name)
+      end
+
+      @parsed_query.query_expression.instance_variable_set(
+        "@list",
+        SQLParser::Statement::SelectList.new(sql_columns)
+      )
+
+      # binding.pry
+    end
+  end
+end
