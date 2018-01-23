fb29f0e180f2c9c607a892b1f6cd53d093ecf323
Vlad Stoica <vlad96stoica@gmail.com>
2018-01-23 11:32:17 +0000

    Extract data in compile

---
+++ spec/sql_assess/database_data_extractor_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+RSpec.describe SqlAssess::DatabaseDataExtractor do
+  let(:connection) { SqlAssess::DatabaseConnection.new }
+  subject { described_class.new(connection) }
+
+  context "with a single table" do
+    before do
+      connection.query('CREATE TABLE table1 (id integer);')
+      connection.query('INSERT INTO table1 (id) values(1);')
+      connection.query('INSERT INTO table1 (id) values(2);')
+    end
+
+    it "returns the correct answer" do
+      expect(subject.run).to eq([
+        {
+          name: "table1",
+          columns: ["id"],
+          data: [{ "id" => 1 }, { "id" => 2 }]
+        }
+      ])
+    end
+  end
+end
