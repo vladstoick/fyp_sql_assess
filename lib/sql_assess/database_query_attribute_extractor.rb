930d6044b6427c35d0aa498498a085fa2c1730f0
Vlad Stoica <vlad96stoica@gmail.com>
2018-01-25 10:57:20 +0000

    Also send order by in database query attribute extractor

--- lib/sql_assess/database_query_attribute_extractor.rb
+++ lib/sql_assess/database_query_attribute_extractor.rb
@@ -9,7 +9,11 @@ module SqlAssess
         columns: {
           instructor_columns: Parsers::Columns.new(instructor_sql_query).columns,
           student_columns: Parsers::Columns.new(student_sql_query).columns,
-        }
+        },
+        order_by: {
+          instructor_order_by: Parsers::OrderBy.new(instructor_sql_query).order,
+          student_order_by: Parsers::OrderBy.new(student_sql_query).order
+        },
       }
     end
   end
