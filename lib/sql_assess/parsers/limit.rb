7fb8d3dc7cf824345a810a76b05882f589c6fef8
Vlad Stoica <vlad96stoica@gmail.com>
2018-02-23 17:13:26 +0200

    Add limit parsing

---
+++ lib/sql_assess/parsers/limit.rb
@@ -0,0 +1,17 @@
+module SqlAssess::Parsers
+  class Limit < Base
+    def limit
+      if @parsed_query.query_expression&.table_expression&.limit_clause.present?
+        {
+          limit: @parsed_query.query_expression&.table_expression&.limit_clause.limit,
+          offset: @parsed_query.query_expression&.table_expression&.limit_clause.offset || 0
+        }
+      else
+        {
+          limit: "inf",
+          offset: 0
+        }
+      end
+    end
+  end
+end
