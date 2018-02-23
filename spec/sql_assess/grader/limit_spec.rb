7fea963dd366fe4b57d8b1ef1180077c3f712eaf
Vlad Stoica <vlad96stoica@gmail.com>
2018-02-23 17:18:26 +0200

    Add limit grading

---
+++ spec/sql_assess/grader/limit_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+RSpec.describe SqlAssess::Grader::Limit do
+  subject do
+    described_class.new(
+      student_attributes: student_limit,
+      instructor_attributes: instructor_limit
+    )
+  end
+
+  context "same limit and offsert" do
+    let(:student_limit) { { "limit": 1, "offset": 0 } }
+    let(:instructor_limit) { { "limit": 1, "offset": 0 } }
+
+    it { expect(subject.rounded_grade).to eq(1) }
+  end
+
+  context "same limit but different offset" do
+    let(:student_limit) { { "limit": 1, "offset": 1 } }
+    let(:instructor_limit) { { "limit": 1, "offset": 0 } }
+
+    it { expect(subject.rounded_grade).to eq(0) }
+  end
+
+  context "different limit but same offset" do
+    let(:student_limit) { { "limit": 2, "offset": 0 } }
+    let(:instructor_limit) { { "limit": 1, "offset": 0 } }
+
+    it { expect(subject.rounded_grade).to eq(0) }
+  end
+end
