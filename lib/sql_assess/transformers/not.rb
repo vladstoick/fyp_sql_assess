704ebe8f7f9485e21f56416702f5ff051efdb9a0
Vlad Stoica <vlad96stoica@gmail.com>
2018-04-02 22:09:30 +0300

    Update not statements

--- lib/sql_assess/transformers/not.rb
+++ lib/sql_assess/transformers/not.rb
@@ -22,25 +22,25 @@ module SqlAssess::Transformers
           transform_where_query(statement.right)
         )
       elsif statement.is_a?(SQLParser::Statement::Not)
-        transform_not_query(statement)
+        transform_not(statement)
       else
         statement
       end
     end
 
-    def transform_not_query(statement)
+    def transform_not(not_statement)
       # Greater
-      if statement.value.is_a?(SQLParser::Statement::Greater)
-        SQLParser::Statement::LessOrEquals.new(statement.value.left, statement.value.right)
-      elsif statement.value.is_a?(SQLParser::Statement::GreaterOrEquals)
-        SQLParser::Statement::Less.new(statement.value.left, statement.value.right)
+      if not_statement.value.is_a?(SQLParser::Statement::Greater)
+        SQLParser::Statement::LessOrEquals.new(not_statement.value.left, not_statement.value.right)
+      elsif not_statement.value.is_a?(SQLParser::Statement::GreaterOrEquals)
+        SQLParser::Statement::Less.new(not_statement.value.left, not_statement.value.right)
       # Less
-      elsif statement.value.is_a?(SQLParser::Statement::Less)
-        SQLParser::Statement::GreaterOrEquals.new(statement.value.left, statement.value.right)
-      elsif statement.value.is_a?(SQLParser::Statement::LessOrEquals)
-        SQLParser::Statement::Greater.new(statement.value.left, statement.value.right)
+      elsif not_statement.value.is_a?(SQLParser::Statement::Less)
+        SQLParser::Statement::GreaterOrEquals.new(not_statement.value.left, not_statement.value.right)
+      elsif not_statement.value.is_a?(SQLParser::Statement::LessOrEquals)
+        SQLParser::Statement::Greater.new(not_statement.value.left, not_statement.value.right)
       else
-        statement
+        not_statement
       end
     end
   end
