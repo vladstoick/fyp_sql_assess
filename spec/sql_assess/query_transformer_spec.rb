bad391323988c9ff0b44a7700dcbb98391025a95
Vlad Stoica <vlad96stoica@gmail.com>
2018-04-07 13:39:27 +0300

    Hackerrank specs

--- spec/sql_assess/query_transformer_spec.rb
+++ spec/sql_assess/query_transformer_spec.rb
@@ -4,17 +4,41 @@ require 'yaml'
 RSpec.describe SqlAssess::QueryTransformer do
   subject { described_class.new(connection) }
 
-  yaml = YAML.load_file("spec/fixtures/transformer_integration_tests.yml")
+  # yaml = YAML.load_file("spec/fixtures/transformer_integration_tests.yml")
 
-  yaml.each do |test|
-    it "transform #{test['query']} to #{test['expected_result']}" do
-      # Seed date
-      connection.multiple_query(test["schema"])
-      # Check if queries from file are correct
-      connection.query(test["query"])
-      connection.query(test["expected_result"])
-      # Check transformation
-      expect(subject.transform(test["query"])).to eq(test["expected_result"])
+  # yaml.each do |test|
+  #   it "transform #{test['query']} to #{test['expected_result']}" do
+  #     # Seed data
+  #     connection.multiple_query(test["schema"])
+  #     # Check if queries from file are correct
+  #     connection.query(test["query"])
+  #     connection.query(test["expected_result"])
+  #     # Check transformation
+  #     expect(subject.transform(test["query"])).to eq(test["expected_result"])
+  #   end
+  # end
+
+  yaml2 = YAML.load_file("spec/fixtures/transformer_hacker_rank_integration_tests.yml")
+
+  yaml2.each do |test|
+    if test["support"] == false
+      xit "#{test['name']}" do
+        execute_query(test)
+      end
+    else
+      it "#{test['name']}: transform #{test['query'].squish} to #{test['expected_result'].squish}" do
+        execute_query(test)
+      end
     end
+  end
+
+  def execute_query(test)
+    # Seed data
+    connection.multiple_query(test["schema"])
+    # Check if queries from file are correct
+    connection.query(test["query"])
+    connection.query(test["expected_result"])
+    # Check transformation
+    expect(subject.transform(test["query"])).to eq(test["expected_result"].squish)
   end
 end
---
+++ spec/sql_assess/transformers/ambigous_columns_where_spec.rb
@@ -0,0 +1,78 @@
+require "spec_helper"
+
+RSpec.describe SqlAssess::Transformers::AmbigousColumnsWhere do
+  subject { described_class.new(connection) }
+
+  before do
+    connection.query("CREATE TABLE table1 (id1 integer, id2 integer)")
+    connection.query("CREATE TABLE table2 (id3 integer, id4 integer)")
+  end
+
+  context "with no where clause" do
+    let(:query) do
+      <<-SQL.squish
+        SELECT `table1`.`id`, `table1`.`id2`
+        FROM `table1`
+      SQL
+    end
+
+    it "doesn't change the query" do
+      expect(subject.transform(query)).to eq(query)
+    end
+  end
+
+  context "with WHERE clause but no ambigous column" do
+    let(:query) do
+      <<-SQL.squish
+        SELECT `table1`.`id`, `table1`.`id2`
+        FROM `table1`
+        WHERE `table1`.`id1` > 1
+      SQL
+    end
+
+    it "doesn't change the query" do
+      expect(subject.transform(query)).to eq(query)
+    end
+  end
+
+  context "with where clause but with an ambigous column" do
+    let(:query) do
+      <<-SQL.squish
+        SELECT `table1`.`id`, `table1`.`id2`
+        FROM `table1`
+        WHERE `id1` > 1
+      SQL
+    end
+
+    it "changes the query" do
+      expect(subject.transform(query)).to eq(
+        <<-SQL.squish
+          SELECT `table1`.`id`, `table1`.`id2`
+          FROM `table1`
+          WHERE `table1`.`id1` > 1
+        SQL
+      )
+    end
+  end
+
+  context "with where clause but with an ambigous column" do
+    let(:query) do
+      <<-SQL.squish
+        SELECT `table1`.`id`, `table1`.`id2`
+        FROM `table1`
+        WHERE `id1` > 1 AND `id2` > 1
+      SQL
+    end
+
+    it "changes the query" do
+      expect(subject.transform(query)).to eq(
+        <<-SQL.squish
+          SELECT `table1`.`id`, `table1`.`id2`
+          FROM `table1`
+          WHERE (`table1`.`id1` > 1 AND `table1`.`id2` > 1)
+        SQL
+      )
+    end
+  end
+
+end
